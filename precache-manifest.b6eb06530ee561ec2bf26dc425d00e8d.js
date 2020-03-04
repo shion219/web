888b6de25a5e1b784365d0860771c0f5242aaad7
shion219 <48423706+shion219@users.noreply.github.com>
2020-03-04 21:06:41 +0900

    Updates

---
+++ precache-manifest.b6eb06530ee561ec2bf26dc425d00e8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8f6b910e256cafc382d98c821a2382",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "50186878aac5dd5f2058",
+    "url": "/web/static/css/main.8d3edc43.chunk.css"
+  },
+  {
+    "revision": "6ce31b2486539ef696df",
+    "url": "/web/static/js/2.f9540ce4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/web/static/js/2.f9540ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50186878aac5dd5f2058",
+    "url": "/web/static/js/main.d06b2412.chunk.js"
+  },
+  {
+    "revision": "f0421e3d03d9987c29b7",
+    "url": "/web/static/js/runtime-main.cb6c3fcb.js"
+  }
+]);
